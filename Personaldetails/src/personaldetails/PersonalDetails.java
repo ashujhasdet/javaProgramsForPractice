0f8d01e6ccd24a88474ac7bc8e397670d7d43e02
AK <145312210+ashujhasdet@users.noreply.github.com>
2023-09-22 15:16:50 +0530

    Add files via upload

---
+++ Personaldetails/src/personaldetails/PersonalDetails.java
@@ -0,0 +1,20 @@
+package personaldetails;
+
+public class PersonalDetails {
+	public static void main(String[] args) {
+		System.out.println("main starts");
+		String name = "Aashutosh Jha";
+		int age = 26;
+		String roll_no = "15CS001";
+		float height = 5.7f;
+		int year_of_passout = 2019;
+		
+		System.out.println("my name is " + name);
+		System.out.println("my age is "+ age);
+		System.out.println("my roll number is = "+ roll_no);
+		System.out.println("my height is " + height);
+		System.out.println("passout year is " +  year_of_passout);
+		System.out.println("main ends");
+	}
+
+}
