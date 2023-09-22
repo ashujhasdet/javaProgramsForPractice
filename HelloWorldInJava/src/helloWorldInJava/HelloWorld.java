06b930dd9b5b8f666f098d1cb9d8e2835ca9af93
ashuk <ashuk@DESKTOP-EOJF1B7>
2023-09-22 14:50:28 +0530

    Initial Commit

---
+++ HelloWorldInJava/src/helloWorldInJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloWorldInJava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.println("main starts");	
+		System.out.println("Hello World");
+	    System.out.println("main ends");
+		}
+
+}
---
+++ HelloWorldInJava/src/helloWorldInJava/HelloWorldWithouMain.java
@@ -0,0 +1,7 @@
+package helloWorldInJava;
+
+public class HelloWorldWithouMain {
+	
+	// Un-comment below line and run to check what happens if you are not using  main method
+    // System.out.println("Hello World");
+}
